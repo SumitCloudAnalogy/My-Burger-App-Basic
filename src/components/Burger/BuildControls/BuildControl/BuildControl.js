852514c5e278727ecc29fcfce3844c32e7d9c5db
Vishal Sharma <vishal.sharma@cloudanalogy.com>
2019-06-26 17:02:35 +0530

    commit on order summary

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,11 +2,11 @@ import React from 'react';
 import classes from './BuildControl.css'
 
 const buildControl = (props) => {
-    // console.log("BuildControl.js", props);
+   // console.log("BuildControl.js", props);
     return (
         <div className={classes.BuildControl}>
             <div className={classes.Label}>{props.label}</div>
-            <button className={classes.Less}>Less</button>
+            <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>Less</button>
             <button className={classes.More} onClick={props.added}>More</button>
         </div>
         )
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -14,14 +14,19 @@ const buildControls = (props) =>  {
     // console.log("BuildControls.js", props);
     return (
         <div className={classes.BuildControls}>
+            <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
             <h1>Use This</h1>
             <img src={logo} alt="" className={classes.logo}/>
             {controls.map(ctrl => (
                 <BuildControl
                     key={ctrl.label} label={ctrl.label}
-                    added={()=> props.ingredientsAdded(ctrl.type)}
+                    added={()=> props.ingredientAdded(ctrl.type)}
+                    removed={()=> props.ingredientRemoved(ctrl.type)}
+                    disabled={props.disabled[ctrl.type]}
                 />
             ))}
+            <button className={classes.OrderButton}
+            disabled={!props.purchasable}>ORDER NOW</button>
         </div>
     )
 } ;
