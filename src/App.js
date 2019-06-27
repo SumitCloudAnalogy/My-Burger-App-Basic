14af4c60cb334fc7b4e1256fba45040bb0a167e7
Sumit Kumar <sumit.kumar@cloudanalogy.com>
2019-06-27 15:26:44 +0530

    commit by sumit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-import Layout from './components/Layout/Layout'
+import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 
 class App extends Component{
   render() {
+      console.log("App.js");
     return (
         <div>
           <Layout>
