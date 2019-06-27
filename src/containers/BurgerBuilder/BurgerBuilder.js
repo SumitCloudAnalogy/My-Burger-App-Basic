9590e68496ae7cd2c7a2d4d943911f4d218a3de7
Sumit Kumar <sumit.kumar@cloudanalogy.com>
2019-06-27 19:25:45 +0530

    firebase-connection and post request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from  '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -82,7 +83,27 @@ class BurgerBuilder extends Component{
     };
 
     purchaseContinueHandler = () => {
-      alert("You continue!");
+     // alert("You continue!");
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Max Schwarzmuller',
+                address: {
+                    street: 'Teststreet 1',
+                    zipCode: '41351',
+                    country: 'India'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => {
+                console.log("Response: ", response);
+            }).catch(error => {
+            console.log("Error: ",error);
+        })
     };
     render() {
         const disabledInfo = {
